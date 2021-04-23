5ccfdb2f8f32881840d2f6fb81f40a9e678d5eea
kalinga <kbog1983@gmail.com>
2021-04-23 12:34:21 -0700

    commit first client-node1

---
+++ config/db_properties.js
@@ -0,0 +1,6 @@
+ module.exports = {
+    host:"localhost",
+    user:"root",
+    password:"yourpasswd",
+    database:"mydb"
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+
+var app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+    next();
+});
+
+
+var productAPI = require("./controllers/product.controller");
+app.use("/api/products",productAPI);
+
+app.listen(8080);
+console.log("Server up and running on port 8080");
